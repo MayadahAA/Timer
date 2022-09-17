9f90907208cf4d1097abbdb77f7672edf3329a2f
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2022-09-17 13:57:22 +0300

    commit

--- timer.js
+++ timer.js
@@ -1,13 +1,80 @@
+/*
 const el = document.querySelector(".clock");
 const mindiv = document.querySelector(".mins");
 const secdiv = document.querySelector(".secs");
-const startbtn = document.querySelector(".start");
-
+const startBtn = document.querySelector(".start");
 localStorage.setItem("btn", "focus");
 
 let initial, totalsecs, perc, paused, mins, seconds;
 
-startbtn.addEventListener("click", () => {
+startBtn.addEventListener("click", () => {
+  let btn = localStorage.getItem("btn");
+
+  if (btn === "focus") {
+    mins = +localStorage.getItem("focusTime");
+  } else {
+    mins = +localStorage.getItem("breakTime");
+  }
+
+  seconds = mins * 60;
+  totalsecs = mins * 60;
+  setTimeout(decremenT(), 60);
+  startBtn.style.transform = "scale(0)";
+  paused = false;
+});
+
+function decremenT() {
+  mindiv.textContent = Math.floor(seconds / 60);
+  secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+
+  function decremenT() {
+    mindiv.textContent = Math.floor(seconds / 60);
+    secdiv.textContent = seconds % 60 ;
+
+    if (seconds > 0) {
+      perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
+      setProgress(perc);
+      if (circle.classList.contains("danger")) {
+        circle.classList.remove("danger")
+        
+      }
+        seconds--;
+        initial = window.setTimeout("decremenT()", 1000);
+        if (seconds < 10) {
+          circle.classList.add("danger");
+          
+        }
+     } else {
+        mins = 0;
+        seconds = 0;
+        let btn = localStorage.getItem("btn");
+
+        if (btn === "focus") {
+            startBtn.textContent = "start break";
+            startBtn.classList.add("break");
+            localStorage.setItem("btn", "break");
+        } else {
+            startBtn.classList.remove("break");
+            startBtn.textContent = "start focus";
+            localStorage.setItem("btn", "focus");
+        }
+        startBtn.style.transform = "scale(1)";
+      }
+    }
+  } */
+
+  const el = document.querySelector(".clock");
+  const bell = document.querySelector("audio");
+  
+  const mindiv = document.querySelector(".mins");
+  const secdiv = document.querySelector(".secs");
+  
+  const startBtn = document.querySelector(".start");
+  localStorage.setItem("btn", "focus");
+  
+  let initial, totalsecs, perc, paused, mins, seconds;
+  
+  startBtn.addEventListener("click", () => {
     let btn = localStorage.getItem("btn");
   
     if (btn === "focus") {
@@ -19,31 +86,41 @@ startbtn.addEventListener("click", () => {
     seconds = mins * 60;
     totalsecs = mins * 60;
     setTimeout(decremenT(), 60);
-    startbtn.style.transform = "scale(0)";
+    startBtn.style.transform = "scale(0)";
     paused = false;
   });
   
   function decremenT() {
     mindiv.textContent = Math.floor(seconds / 60);
-    secdiv.textContent = seconds % 60 ;
-
-    if (seconds > 0) {
-        seconds--;
-        initial = window.setTimeout("decremenT()", 1000);
-     } else {
-        mins = 0;
-        seconds = 0;
-        let btn = localStorage.getItem("btn");
-
-        if (btn === "focus") {
-            startbtn.textContent = "start break";
-            startbtn.classList.add("break");
-            localStorage.setItem("btn", "break");
-        } else {
-            startbtn.classList.remove("break");
-            startbtn.textContent = "start focus";
-            localStorage.setItem("btn", "focus");
-        }
-        startbtn.style.transform = "scale(1)";
-        }
+    secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+    if (circle.classList.contains("danger")) {
+      circle.classList.remove("danger");
     }
+  
+    if (seconds > 0) {
+      perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
+      setProgress(perc);
+      seconds--;
+      initial = window.setTimeout("decremenT()", 1000);
+      if (seconds < 10) {
+        circle.classList.add("danger");
+      }
+    } else {
+      mins = 0;
+      seconds = 0;
+      bell.play();
+      let btn = localStorage.getItem("btn");
+  
+      if (btn === "focus") {
+        startBtn.textContent = "start break";
+        startBtn.classList.add("break");
+        localStorage.setItem("btn", "break");
+      } else {
+        startBtn.classList.remove("break");
+        startBtn.textContent = "start focus";
+        localStorage.setItem("btn", "focus");
+      }
+      startBtn.style.transform = "scale(1)";
+    }
+  }
+  
